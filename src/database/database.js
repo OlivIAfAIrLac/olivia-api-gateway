e487a2576dd3e1ea59b04619bdad7b3e9ad6252b
nachomartinez3010 <nachomartinez3010@gmail.com>
2023-10-18 16:12:42 -0600

    changing db string connection

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 require('dotenv').config()
 
 // const uri = process.env.DB_URI ? process.env.DB_URI : '' test
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@oliviacluster.w2lhg7r.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-xxndnok-shard-00-00.w2lhg7r.mongodb.net:27017,ac-xxndnok-shard-00-01.w2lhg7r.mongodb.net:27017,ac-xxndnok-shard-00-02.w2lhg7r.mongodb.net:27017/?ssl=true&replicaSet=atlas-134xus-shard-0&authSource=admin&retryWrites=true&w=majority`
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@oliviacluster.w2lhg7r.mongodb.net/?retryWrites=true&w=majority`
 
 export const db = async () => {
     try {
